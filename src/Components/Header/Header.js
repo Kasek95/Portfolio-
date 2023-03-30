73d4e886e3d97cba0d4d9cc45ee1923633123e8c
Kasek95 <pawel.kasek62@gmail.com>
2023-03-31 01:25:53 +0200

    Make a Header and navigation

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,32 @@
+import "./header.scss"
+import React, {useState} from "react";
+import {Link} from "react-router-dom"
+
+const Header = () => {
+    const [isDisplay, setIsDisplay] = useState(false)
+
+    return (
+        <>
+            <header>
+                <Link className={"logo"} to={"/"}>Portfolio.</Link>
+
+                <nav className={"navigation"}>
+                   <div className={"icon-container"}>
+                       <i onClick={ ()=> setIsDisplay(true)} className={isDisplay ? "fa-solid fa-bars hide " : "fa-solid fa-bars bars"}></i>
+                       <i onClick={ () => setIsDisplay(false)} className={isDisplay ? "fa-solid fa-x bars show" : "fa-solid fa-x hide"}></i>
+                   </div>
+
+                    <div className={isDisplay ? "container-link show" : "container-link"}>
+                        <Link className={"link"} to={"Skills"}>Skills</Link>
+                        <Link className={"link"} to={"Portfolio"}>Projects</Link>
+                        <Link className={"link"} to={"Contact"}>Contact</Link>
+                    </div>
+
+                </nav>
+            </header>
+        </>
+    )
+
+}
+
+export default Header;
