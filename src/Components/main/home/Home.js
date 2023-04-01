ab6e27507e99b4b71b8757790493aaf1398e637b
Kasek95 <pawel.kasek62@gmail.com>
2023-04-01 03:11:13 +0200

    Dodano stylowania

--- src/Components/main/home/Home.js
+++ src/Components/main/home/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
         <section className={"home"}>
 
             <div className={"home-content"}>
-                <h1>Hi, I'm Paweł Kąsek</h1>
+                <h1>Hi, I&apos;m Paweł Kąsek</h1>
                 <h3>Frontend Developer</h3>
                 <p>I started my adventure with programming almost a year ago.
                     During this time I have been learning and increasing my tech stack.
@@ -16,14 +16,18 @@ const Home = () => {
                 </p>
                 <a className={"btnHire"} href={"#"}>Hire Me!</a>
                 <div className={"home-sci"}>
-                    <a href={"https://www.facebook.com/paolo.wielki/"} target={"_blank"}><i className='bx bxl-facebook'></i></a>
+                    <a href={"https://www.facebook.com/paolo.wielki/"} target={"_blank"}   rel="noreferrer"><i className='bx bxl-facebook'></i></a>
                     <a href={""}><i className='bx bxl-twitter'></i></a>
-                    <a href={"https://www.linkedin.com/in/pawe%C5%82-k%C4%85sek-256404266/"} target={"_blank"}><i className='bx bxl-linkedin'></i></a>
+                    <a href={"https://www.linkedin.com/in/pawe%C5%82-k%C4%85sek-256404266/"} rel="noreferrer" target={"_blank"}><i className='bx bxl-linkedin'></i></a>
                 </div>
             </div>
 
             <div className={"home-img"}>
-                <div className={"glowing-circle"}></div>
+
+                <div className={"glowing-circle"}>
+                    <span></span>
+                    <span></span>
+                </div>
             </div>
         </section>
     )
